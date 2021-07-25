060a2bb2372c2861045890508d65f1730ea4a0de
Julio Campos <julio.cesar.campos.salcedo@everis.nttdata.com>
2021-07-25 12:58:42 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'certification';
+export class AppComponent  {
+  controlForm: FormGroup;
+  name = 'Angular';
+  zipCodes : [string] = localStorage.getItem("listZipcode")? JSON.parse(localStorage.getItem("listZipcode")) : [];
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.initForm();
+  }
+
+  initForm() {
+    this.controlForm = this.fb.group({
+      zipCode: ["", [
+        Validators.required, 
+        Validators.minLength(5), 
+        Validators.pattern("^[0-9]{5}(?:-[0-9]{4})?$")]],
+    });
+  }
+
+  onChange() {
+    console.log(this.controlForm)
+  }
+
+  listZipCode () {
+    this.zipCodes.push(this.controlForm.controls.zipCode.value);
+    localStorage.setItem("listZipcode", JSON.stringify(this.zipCodes));
+    this.controlForm.reset();
+  }
 }
